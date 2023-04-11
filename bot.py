0ae11da5aaddc9063e92c2ac12e7258a77d14630
Harnonliu <127935243+Harnonliu@users.noreply.github.com>
2023-04-12 00:57:23 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -50,3 +50,71 @@ def default_message_handler(message):
 
 # Start bot
 bot.infinity_polling()
+##########################################################################
+
+class ProcessInfo:
+    def __init__(self, pid):
+        self.process = psutil.Process(pid)
+
+    def get_pid(self):
+        return self.process.pid
+
+    def get_name(self):
+        return self.process.name()
+
+    def get_create_time(self):
+        return self.process.create_time()
+
+    def get_run_time(self):
+        return psutil.Process().cpu_times().user - self.process.cpu_times().user
+
+    def get_cpu_percent(self):
+        return self.process.cpu_percent()
+
+    def get_memory_percent(self):
+        return self.process.memory_percent()
+
+    def get_network_usage(self):
+        connections = self.process.connections()
+        total_sent = 0
+        total_recv = 0
+        for conn in connections:
+            total_sent += conn.sent
+            total_recv += conn.recv
+        return (total_sent, total_recv)
+
+class ProcessClassifier:
+    def __init__(self):
+        self.games = ["game1", "game2", "game3"] # Replace with actual game names
+        self.platforms = ["steam", "epicgames", "origin"] # Replace with actual game platform names
+
+    def classify_processes(self):
+        game_processes = []
+        for process in psutil.process_iter(['pid', 'name', 'cpu_percent', 'memory_percent']):
+            name = process.info['name']
+            cpu_percent = process.info['cpu_percent']
+            memory_percent = process.info['memory_percent']
+            if any(game_name in name.lower() for game_name in self.games):
+                game_processes.append(process)
+            elif any(platform_name in name.lower() for platform_name in self.platforms):
+                game_processes.append(process)
+            elif cpu_percent > 30 or memory_percent > 30:
+                game_processes.append(process)
+        return game_processes
+
+    def terminate_process_by_name(self, process_name):
+        found = False
+        for process in psutil.process_iter(['pid', 'name']):
+            if process.info['name'] == process_name:
+                process.terminate()
+                found = True
+                break
+        return found
+
+    def terminate_process_by_pid(self, pid):
+        try:
+            process = psutil.Process(pid)
+            process.terminate()
+            return True
+        except psutil.NoSuchProcess:
+            return False
